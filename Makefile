17367ab98769b4fea770936110d45de7b2801fa2
Aswin Udhayakumar <aswiniu03@gmail.com>
2023-09-13 22:10:07 +0530

    Add docker file and webserver

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
 run:
 	go run main.go
+
+dep:
+	go mod tidy
+	go mod vendor
+
+build:
+	docker build -t k8scourse:v1 .
+
+run-build:
+	docker run -d -p 3000:3000 --name webserver k8scourse:v1
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	router := chi.NewRouter()
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello gopher!"))
+	})
+
+	http.ListenAndServe(":3000", router)
 }
